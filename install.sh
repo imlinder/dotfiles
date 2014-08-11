9f5cc14f84f2480171a6e2d55858034ee9e29ab5
Johan Linder <johan.linder@flatmate.se>
2014-08-11 09:07:19 +0200

    Add tmux.conf

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim profile"	  # list of files/folders to symlink in homedir
+files="vimrc tmux.conf vim profile"	  # list of files/folders to symlink in homedir
 
 ##########
 
