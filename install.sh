5c16654a82e7f29bde783e74f9b6cb601ee99703
Johan Linder <linder91@gmail.com>
2014-12-16 21:00:41 +0100

    Remove unused profile

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc vimrc tmux.conf vim profile"	  # list of files/folders to symlink in homedir
+files="zshrc vimrc tmux.conf vim" # list of files/folders to symlink in homedir
 
 ##########
 
