189062f8203b6621de5fc0ecd0e8207f89ee8889
Johan Linder <linder91@gmail.com>
2014-12-16 20:57:14 +0100

    Add oh-my-zsh as submodule

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc tmux.conf vim profile"	  # list of files/folders to symlink in homedir
+files="zshrc vimrc tmux.conf vim profile"	  # list of files/folders to symlink in homedir
 
 ##########
 
