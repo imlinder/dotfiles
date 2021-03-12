e526fa6d89c675aac1d5c838aedc407c5d93c54d
Johan Linder <johan@fmca.se>
2021-03-12 17:04:25 +0100

    Hammerspoon: ctrlescape + more lunette bindings

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,8 +1,18 @@
+---------------------
 -- Reload Hammerspoon
+---------------------
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 hs.alert.show("Config reloaded")
 
+----------------
+-- ControlEscape
+----------------
+hs.loadSpoon("ControlEscape"):start()
+
+--------------------------
+-- Applications on hotkeys
+--------------------------
 hs.hotkey.bind({"cmd", "ctrl"}, "O", function()
 	hs.application.launchOrFocus("iTerm")
 end)
@@ -11,7 +21,13 @@ hs.hotkey.bind({"cmd", "ctrl"}, "I", function()
 	hs.application.launchOrFocus("Google Chrome")
 end)
 
+hs.hotkey.bind({"cmd", "ctrl"}, "P", function()
+	hs.application.launchOrFocus("Slack")
+end)
+
+------------------------------------------
 -- https://github.com/scottwhudson/Lunette
+------------------------------------------
 hs.loadSpoon("Lunette")
 customBindings = {
 	center = {
@@ -48,9 +64,15 @@ customBindings = {
 	},
 	nextThird = false,
 	prevThird = false,
-	enlarge = false,
-	shrink = false,
-	undo = false,
+	enlarge = {
+		{{ "cmd", "ctrl" }, "["}
+	},
+	shrink = {
+		{{ "cmd", "ctrl" }, "]"}
+	},
+	undo = {
+		{{ "cmd", "ctrl"}, "Z"}
+	},
 	redo = false,
 }
 spoon.Lunette:bindHotkeys(customBindings)
