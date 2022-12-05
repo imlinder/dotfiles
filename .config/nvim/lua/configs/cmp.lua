53081c41b2a4c7be14c97b26d9aa8c7b37fe5e52
Johan Linder <johan@fmca.se>
2022-12-05 19:04:00 +0100

    Add missing nvim config

---
+++ .config/nvim/lua/configs/cmp.lua
@@ -0,0 +1,41 @@
+local cmp = require'cmp'
+
+cmp.setup({
+  sources = cmp.config.sources({
+    { name = 'luasnip' }, -- For luasnip users.
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'buffer' },
+  }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+})
+
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'cmdline' }
+  },
+})
+
