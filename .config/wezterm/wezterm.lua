51b90013d3e19869724ca2d9152d213119c9b293
Johan Linder <johan@fmca.se>
2024-01-12 16:58:55 +0100

    Kanagawa color scheme for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,8 +19,33 @@ wezterm.on('window-config-reloaded', function(window, pane)
 end)
 
 return {
-  font = wezterm.font ('FiraCode Nerd Font Mono', { weight = 450, italic = false}), -- 450 is the weight for the Retina weight apparently
-  color_scheme = "Catppuccin Mocha",
+  font = wezterm.font ('FiraCode Nerd Font Mono', { weight = 450}), -- 450 is the weight for the Retina weight apparently
+  font_rules = {
+
+  },
+  -- color_scheme = "Catppuccin Mocha",
   hide_tab_bar_if_only_one_tab = true,
   -- window_decorations = "RESIZE",
+
+  force_reverse_video_cursor = true,
+
+  -- Kanagawa color scheme
+  colors = {
+    foreground = "#dcd7ba",
+    background = "#1f1f28",
+
+    cursor_bg = "#c8c093",
+    cursor_fg = "#c8c093",
+    cursor_border = "#c8c093",
+
+    selection_fg = "#c8c093",
+    selection_bg = "#2d4f67",
+
+    scrollbar_thumb = "#16161d",
+    split = "#16161d",
+
+    ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+    brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+    indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+  },
 }
