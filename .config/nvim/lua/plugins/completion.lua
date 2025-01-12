7be6662fee5327524d283b3a30e1a4df661931e8
Johan Linder <johan@fmca.se>
2025-01-12 13:47:01 +0100

    Enable signature help in blink.cmp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -5,11 +5,11 @@ return {
     version = '*',
     opts = {
       keymap = { preset = 'default' },
-
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
       },
+      signature = { enabled = true },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
