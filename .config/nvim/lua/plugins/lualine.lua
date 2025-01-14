e1dbf39fd39e2db820eed49eee1bf35e04f8f91d
Johan Linder <johan@fmca.se>
2025-01-14 16:33:41 +0100

    Replace nvim-web-devicons with mini.icons

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { "echasnovski/mini.icons" },
     opts = {
       options = {
         icons_enabled = true,
--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,20 @@
 return {
   {
     'kyazdani42/nvim-tree.lua',
+    dependencies = {
+      "echasnovski/mini.icons",
+      opts = {},
+      lazy = true,
+      specs = {
+        { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+      },
+      init = function()
+        package.preload["nvim-web-devicons"] = function()
+          require("mini.icons").mock_nvim_web_devicons()
+          return package.loaded["nvim-web-devicons"]
+        end
+      end,
+    },
     opts = {
       hijack_netrw = false,
       update_focused_file = {
